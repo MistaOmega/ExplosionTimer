b7965aef62e036c7eb3636efb1e35baa9211c85c
MistaOmega <jack.nash6@gmail.com>
2024-01-15 19:18:03 +0000

    Initial

---
+++ ExplosionPatch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace ExplosionTimer;
+
+
+public class ExplosionPatch
+{
+    [HarmonyPatch(typeof(CrashedShipExploder), nameof(CrashedShipExploder.SetExplodeTime))]
+    [HarmonyPrefix]
+    static bool Prefix(CrashedShipExploder __instance)
+    {
+        float num = UnityEngine.Random.Range(2.3f, 2.35f);
+        __instance.timeToStartWarning = DayNightCycle.main.timePassedAsFloat;
+        __instance.timeToStartCountdown = __instance.timeToStartWarning + num * 1200f;
+        return false;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace ExplosionTimer
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony harmony = Harmony.CreateAndPatchAll(typeof(ExplosionPatch));
+            Logger.LogInfo($"Harmony has patched all methods for ExplosionPatch");
+        }
+    }
+}
